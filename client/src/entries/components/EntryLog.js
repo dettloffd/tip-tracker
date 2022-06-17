3aa3b93eca1c3523b0210950eef0e5b916975c50
David Dettloff <dettloffd@gmail.com>
2022-06-17 00:23:27 -0400

    Minor change to add keys to entry items through _id

--- client/src/entries/components/EntryLog.js
+++ client/src/entries/components/EntryLog.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 //
 import { Flex, List, Text } from "@chakra-ui/react";
-import { useQuery, useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 import MoonLoader from "react-spinners/MoonLoader";
 //
 import {
@@ -14,7 +14,6 @@ import EntryItem from "./EntryItem";
 const EntryLog = ({ numResults, dateRange }) => {
   const { startDate, endDate } = dateRange;
   const { userId } = useContext(AuthContext);
-  const queryClient = useQueryClient();
 
   const dateRangeProvided = dateRange.startDate !== "";
   let theQueryKey;
@@ -23,8 +22,7 @@ const EntryLog = ({ numResults, dateRange }) => {
   if (dateRangeProvided) {
     theQueryKey = [
       "getAllEntriesByUserIdBetweenDates",
-      // { userId },
-      "34534534534",
+      { userId },
       { startDate, endDate },
     ];
     theQueryFn = getAllEntriesByUserIdBetweenDates;
@@ -33,38 +31,7 @@ const EntryLog = ({ numResults, dateRange }) => {
     theQueryFn = getAllEntriesByUserId;
   }
 
-  // if (dateRangeProvided) {
-  //   theQueryKey = [
-  //     "getAllEntriesByUserIdBetweenDates",
-  //     "12345",
-  //     { startDate, endDate },
-  //   ];
-  //   theQueryFn = getAllEntriesByUserIdBetweenDates;
-  // } else {
-  //   theQueryKey = ["getAllEntriesByUserId", "12345"];
-  //   theQueryFn = getAllEntriesByUserId;
-  // }
-
-  const { data, isLoading, isError, error } = useQuery( theQueryKey, theQueryFn);
-
-  // const { data, isLoading, isError, error } = useQuery( theQueryKey, async () => {
-
-  //   const response = await theQueryFn;
-  //   return response;
-
-
-
-  // },       {
-  //   onSuccess: (data) => {
-  //     // queryClient.invalidateQueries();
-  //     console.log(data);
-  //   },
-  //   onError: (error) => {
-  //     console.log(error);
-  //   },
-  // } ); 
-  
-
+  const { data, isLoading, isError, error } = useQuery(theQueryKey, theQueryFn);
 
   if (isLoading) {
     return (
@@ -75,11 +42,10 @@ const EntryLog = ({ numResults, dateRange }) => {
   }
 
   if (isError) {
-
-    if (error.response.data.message){
-      return (<Text>{error.response.data.message}</Text>)
-    } else{
-      return (<Text>Error has occurred!</Text>)
+    if (error.response.data.message) {
+      return <Text>{error.response.data.message}</Text>;
+    } else {
+      return <Text>Error has occurred!</Text>;
     }
   }
 
@@ -91,9 +57,9 @@ const EntryLog = ({ numResults, dateRange }) => {
         {entriesToDisplay.length > 0 ? (
           <Flex justifyContent={"center"}>
             <List flexBasis={"100%"}>
-              {entriesToDisplay.map((entry, index) => (
+              {entriesToDisplay.map((entry) => (
                 <>
-                  <EntryItem {...entry} key={index} />
+                  <EntryItem {...entry} key={entry._id} />
                 </>
               ))}
             </List>
